7aa93b1fe1c2f31a956df20d7421c019e3ff9a10
alifinrayhan <alifinrayhan@gmail.com>
2025-10-23 12:08:38 +0700

    Menambahkan gambar

--- UI Pertama/app/src/main/java/com/example/uipertama/ui/theme/UIPertama.kt
+++ UI Pertama/app/src/main/java/com/example/uipertama/ui/theme/UIPertama.kt
@@ -1,6 +1,8 @@
 package com.example.uipertama.ui.theme
 
+import android.provider.MediaStore
 import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -8,6 +10,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
@@ -27,9 +30,22 @@ fun Activitaspertama(modifier: Modifier){
         Text(StringResource(R.string.univ),
             fontSize = 22.sp
         spacer(modifier = modifier.height(25.sp)
-        card (modifier = modifier)
-                .fillmaxsize(if)
+        Card (Modifier = Modifier
+                .fillMaxSize(if)
                     .padding(12.dp),
-                    colors = CardDefaults.cardColors()
+                    colors = CardDefaults.cardColours(
+                        containerColor = color.Darkgrey
+                    )){
+                    Row(){
+                        val gambar = painterResource(R.drawable.Logo.umy)
+                        Image(
+                            painter = gambar,
+                            contentDescription = null,
+                            modifier = modifier.size(100.dp).padding(5.sp)
+
+                        )
+                    }
+
+        }
     }
 }
