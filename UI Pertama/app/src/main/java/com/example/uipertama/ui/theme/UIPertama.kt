da04caddcfd570f59c7f639502e0eb33b6658f41
alifinrayhan <alifinrayhan@gmail.com>
2025-10-23 11:18:00 +0700

    Membuat Aktivitas Pertama

---
+++ UI Pertama/app/src/main/java/com/example/uipertama/ui/theme/UIPertama.kt
@@ -0,0 +1,11 @@
+package com.example.uipertama.ui.theme
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+@composable
+fun ActivitasPertama(modifier: Modifier){
+    column (modifier = modifier.padding(top = dp)
+        .fillMaxSize()
+}
