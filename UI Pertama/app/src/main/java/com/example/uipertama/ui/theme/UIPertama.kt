f7d3138d5470c4a1ca1bb5fbc55504601f4e28f7
alifinrayhan <alifinrayhan@gmail.com>
2025-10-23 12:23:01 +0700

    Menambahkan alamat

--- UI Pertama/app/src/main/java/com/example/uipertama/ui/theme/UIPertama.kt
+++ UI Pertama/app/src/main/java/com/example/uipertama/ui/theme/UIPertama.kt
@@ -1,51 +1,97 @@
 package com.example.uipertama.ui.theme
 
-import android.provider.MediaStore
-import androidx.compose.animation.expandHorizontally
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.uipertama.R
 
 @Composable
-fun Activitaspertama(modifier: Modifier){
-    Column ( modifier = modifier.padding(top=110.dp)
-        .fillMaxSize(),
-        horizontalAligment = Alignment.CenterHorizontally
-
-    ){
-        text(stringResource(R.string.prodi),
-            frontSize = 35.sp,
-            fontWeight = FontWeight.Bold)
-        Text(StringResource(R.string.univ),
+fun Activitaspertama(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(top = 110.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.univ),
             fontSize = 22.sp
-        spacer(modifier = modifier.height(25.sp)
-        Card (Modifier = Modifier
-                .fillMaxSize(if)
-                    .padding(12.dp),
-                    colors = CardDefaults.cardColours(
-                        containerColor = color.Darkgrey
-                    )){
-                    Row(){
-                        val gambar = painterResource(R.drawable.Logo.umy)
-                        Image(
-                            painter = gambar,
-                            contentDescription = null,
-                            modifier = modifier.size(100.dp).padding(5.sp)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {
+            Row(
+                modifier = Modifier.padding(5.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val gambar = painterResource(R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
+                space(modifier= modifier.width(38.dp))
+                column() {
+                    text(
+                        stringResource(R.string.nama),
+                        fontSize = 20.sp,
+                        fontFamily =fontFamily.Cursive,
+                        color = Color.White
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    text(
+                        stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        fontFamily =fontFamily.Cursive,
+                        color = Color.White
+                        modifier = Modifier.padding(5.dp)
 
-                        )
-                    }
 
+                    )
+                }
+                box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(5.dp),
+                    contentAlignment = Alignment.BottomEnd
+
+                )
+            }
         }
     }
 }
+
+
