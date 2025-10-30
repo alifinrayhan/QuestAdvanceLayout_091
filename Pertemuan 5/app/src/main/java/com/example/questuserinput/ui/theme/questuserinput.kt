6d1463402249586a8dd683c107663ef21cff9e88
alifinrayhan <alifinrayhan@gmail.com>
2025-10-30 12:47:59 +0700

    membuat hasil dari mengisi data

--- Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
+++ Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
@@ -1,13 +1,17 @@
 package com.example.questuserinput.ui.theme
 
-import android.R
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -20,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.sync.Mutex
@@ -79,12 +84,35 @@ fun FormDataDiri(modifier: Modifier
         )
     }
     Divider(
-        modifier = Modifier.padding(button = dimensionResource(R.dimen.padding_meidum),top = dimensionResource(
+        modifier = Modifier.padding(button = dimensionResource(R.dimen.padding_medium),top = dimensionResource(
             R.dimen.padding_medium
         )),
         thickness = dimensionResource(R.dimen.divider_tipis),
-        color = Color.DarkGrey
+        color = Color.DarkGray
     )
+    Button(
+      modifier = Modifier.padding(
+          bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+              R.dimen.padding_medium
+          )
+      ),
+        Thickness = dimensionResource(R.dimen.padding_medium)
+    )
+    ElevatedCard (
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        colors = CardDefaults.cardColors(containerColor = Color. Black),
+        modifier = Modifier
+            .height(100.dp)
+            .width(300.dp)
+
+    ){
+        Column (modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),){
+            Text(text = "Nama : "+nama, color = Color.White)
+            Text(text = "Gender : "+jenis, color = Color.White)
+            Text(text = "Alamat : "+alamat, color = Color.White)
+
+        }
+    }
 
 
 
