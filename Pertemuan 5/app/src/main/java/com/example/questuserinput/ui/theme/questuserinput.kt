1f7f40e660b3bc62a573c5b59bb9ab2c9764d106
alifinrayhan <alifinrayhan@gmail.com>
2025-10-30 11:42:44 +0700

    membuat variabel untuk mengingat nilai yang di masukan

---
+++ Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
@@ -0,0 +1,18 @@
+package com.example.questuserinput.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import java.lang.reflect.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember() {mutableStateOf("")}
+    var textJK by remember { mutableStateOf("") }
+
+}
