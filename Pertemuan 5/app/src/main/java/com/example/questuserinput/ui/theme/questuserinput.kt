5e230efbfc804f345486c972eec524b1255351fa
alifinrayhan <alifinrayhan@gmail.com>
2025-10-30 11:45:04 +0700

    membuat variabel mengisi data

--- Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
+++ Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
@@ -6,6 +6,7 @@ import java.lang.reflect.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import kotlinx.coroutines.sync.Mutex
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -14,5 +15,11 @@ fun FormDataDiri(modifier: Modifier
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember() {mutableStateOf("")}
     var textJK by remember { mutableStateOf("") }
+
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
+
+
 
 }
