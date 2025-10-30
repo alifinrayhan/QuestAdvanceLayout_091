d608f8e407e48f738cdbc1ea54a62447ef4b7818
alifinrayhan <alifinrayhan@gmail.com>
2025-10-30 11:58:47 +0700

    membuat column mengisi nama

--- Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
+++ Pertemuan 5/app/src/main/java/com/example/questuserinput/ui/theme/questuserinput.kt
@@ -1,11 +1,21 @@
 package com.example.questuserinput.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import java.lang.reflect.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.sync.Mutex
 
 @Composable
@@ -19,6 +29,21 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
+
+    Column(modifier = modifier. padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
 
 
 
